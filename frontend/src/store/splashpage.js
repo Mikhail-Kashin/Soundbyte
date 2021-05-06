7eb8ec4e6ebc635cbb76702423798e907515af01
Mikhail <mikhail.kashin@protonmail.com>
2021-05-05 22:51:02 -0400

    swapping over to modal work

--- frontend/src/store/splashpage.js
+++ frontend/src/store/splashpage.js
@@ -1,6 +1,9 @@
 import { fetch } from './csrf.js';
 
+
 const SONG_FEED = 'SONG_FEED'
+const ADD_SONG = 'ADD_SONG'
+
 
 const getSongFeed = (songs) => ({
   type: SONG_FEED,
@@ -8,7 +11,13 @@ const getSongFeed = (songs) => ({
 })
 
 
-//thunk
+const addSong = (song) => ({
+  type:ADD_SONG,
+  payload:song
+})
+
+
+//thunks
 export const getSongs = () => async dispatch => {
   const res = await fetch(`/api/songs`)
   if (!res.ok){
@@ -17,6 +26,46 @@ export const getSongs = () => async dispatch => {
   const song = await res.data
   await dispatch(getSongFeed(song))
 }
+
+//AWSTHUNK
+export const createSong = (newSong) => async dispatch => {
+  const {userId, songUrl, songName, songGenre} = newSong;
+  const formData = new FormData();
+  formData.append("songName", songName);
+  formData.append("songGenre", songGenre);
+  formData.append("userId", userId);
+  console.log('testing newSONG', newSong)
+  if (songUrl) formData.append('songUrl', songUrl);
+
+  const res = await fetch(`/api/songs/new`, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    body: formData,
+  })
+  console.log('testing......songData', res)
+  const songData = await res.data
+  await dispatch(addSong(songData))
+}
+
+
+//working code NONAWSTHUNK
+// export const createSong = (newSong) => async dispatch => {
+//   const {songUrl, songName, songGenre} = newSong;
+//   // console.log('testing newSONG', newSong)
+//   const res = await fetch(`/api/songs/new`, {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       songUrl,
+//       songName,
+//       songGenre
+//     }),
+//   })
+//   const songData = await res.data
+//   console.log('testing......songData', res.data)
+//   await dispatch(addSong(songData))
+// }
 
 
 
@@ -27,6 +76,10 @@ export default function songReducer(state = initialState, action) {
     case SONG_FEED:
       const newState = {...action.payload}
       return newState
+    case ADD_SONG:
+      let newSongState = Object.assign({}, state)
+      newSongState[action.payload.id] = action.payload
+      return newSongState
     default:
       return state;
   }
