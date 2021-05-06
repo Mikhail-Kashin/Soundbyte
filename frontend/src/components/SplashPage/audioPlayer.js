8292c4b99fa3609ea9b28c725b02c9051a6e2967
Mikhail <mikhail.kashin@protonmail.com>
2021-05-06 18:28:19 -0400

    setup play, pause, next, previous buttons and they are working

---
+++ frontend/src/components/SplashPage/audioPlayer.js
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react';
+import { getSongs } from '../../store/splashpage';
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+const AudioPlayer = () => {
+  const dispatch = useDispatch();
+  const songs = useSelector(state => state.songs)
+  const [songIndex, setSongIndex] = useState(0)
+  const [playing, setPlaying] = useState(true)
+
+  const audio = document.getElementById("audio")
+
+  const songNames = () => {
+    return Object.values(songs).map(song => song.songName)
+  }
+
+  const songUrls = () => {
+    return Object.values(songs).map(song => song.songUrl)
+  }
+
+  let listSongs = songUrls()
+
+  console.log('testingasdfa;sdjflk', listSongs)
+
+
+
+  function playSongs(e) {
+    e.preventDefault()
+    if (playing === true) {
+      setPlaying(false)
+      audio.play()
+    } else {
+      setPlaying(true)
+      audio.pause()
+    }
+  }
+
+  function prevSong(e){
+  e.preventDefault()
+  if (songIndex > 0){
+    setSongIndex(songIndex -1)
+  }else {
+    setSongIndex(listSongs.length - 1)
+  }
+}
+
+function nextSong(e){
+  e.preventDefault()
+  if (songIndex < listSongs.length - 1) {
+    setSongIndex(songIndex + 1)
+  }else {
+    setSongIndex(0)
+  }
+}
+
+if (audio) {
+  audio.addEventListener('ended', function() {
+      if (songIndex < listSongs.length - 1) {
+          setSongIndex(songIndex + 1)
+      } else setSongIndex(0)
+  })}
+
+
+  useEffect(() => {
+    dispatch(getSongs())
+  },[dispatch])
+
+  useEffect(() => {
+    if (audio) audio.play()
+}, [songIndex])
+
+	return (
+    <>
+    <div>
+      <button onClick={e => prevSong(e)}>Previous</button>
+      {playing === true ? <button onClick={e => playSongs(e)}>Play</button> : <button onClick={e => playSongs(e)}>Pause</button>}
+      <button onClick={e => nextSong(e)}>Next</button>
+    </div>
+    <p>
+      <audio
+        id='audio'
+        src={listSongs[songIndex]}
+      />
+    </p>
+    </>
+    );
+}
+
+export default AudioPlayer;
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -5,7 +5,11 @@ import { NavLink } from 'react-router-dom';
 import { getSongs, deleteSong } from '../../store/splashpage';
 import { Modal } from '../../context/Modal'
 import RenderNewSongForm from './newsongform'
+import AudioPlayer from './audioPlayer'
 import './splashPage.css';
+
+
+
 
 function SongPage() {
   // const history = useHistory(); // so that we can redirect after the image upload is successful
@@ -14,10 +18,30 @@ function SongPage() {
   const sessionUser = useSelector(state => state.session.user);
   const [showModal, setShowModal] = useState(false);
 
+  const songNames = () => {
+    return Object.values(songs).map(song => song.songName)
+  }
+
+  const songUrls = () => {
+    return Object.values(songs).map(song => song.songUrl)
+  }
+
+  // const audioElement = new Audio(audio source);
+
+  const songsToPlay = [
+    {
+      title: songNames(),
+      songUrl: songUrls()
+    }
+  ]
+
+
+
+
 
   function removeSongFunc(e, songId){
     e.preventDefault();
-    console.log('test.......>', songId)
+    // console.log('test.......>', songId)
     dispatch(deleteSong(songId))
     dispatch(getSongs())
   }
@@ -29,14 +53,13 @@ function SongPage() {
 
 
 
-
 const renderSongPage = () => {
   if (sessionUser){
     return Object.values(songs).map(song => {
 
       return (
         <div>
-          <NavLink exact to={`/${song.songUrl}`}>{song.songName}</NavLink>
+          <p>{song.songName}</p>
           <button onClick={(e) => removeSongFunc(e, song.id)}>Delete</button>
         </div>
       )
@@ -52,12 +75,22 @@ if (sessionUser){
         {renderSongPage()}
       </div>
       <div>
+        {AudioPlayer()}
+      </div>
+      <div>
       <button onClick={() => setShowModal(true)}>Upload</button>
         {showModal && (
           <Modal onClose={() => setShowModal(false)}>
             <RenderNewSongForm />
           </Modal>
         )}
+      </div>
+      <div>
+      {/* <ReactAudio
+          src='https://soundbyte.s3.amazonaws.com/1620320673909.mp3'
+          width='100%'
+          height='100%'
+          /> */}
       </div>
     </div>
   )
