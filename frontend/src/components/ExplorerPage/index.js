25bcd27aa31c8a22e5371d706fbb13bef961f0ba
Mikhail <mikhail.kashin@protonmail.com>
2021-05-10 12:30:29 -0400

    updated style for explore page to mimic main feed page

--- frontend/src/components/ExplorerPage/index.js
+++ frontend/src/components/ExplorerPage/index.js
@@ -5,7 +5,7 @@ import RenderNewSongForm from '../NewSongForm/newsongform'
 import AudioPlayer from '../SplashPage/audioPlayer'
 import '../SplashPage/splashPage.css';
 import SideBar from '../SplashPage/sidebar'
-import {LogOutComponent} from '../LoginFormModal'
+import {LogOutComponent} from '../SplashPage/logoutbutton'
 import { RenderSongPage } from '../SplashPage/renderSongPage'
 import LoginFormModal from'../LoginFormModal'
 import SignupFormPage from '../SignupFormPage'
@@ -14,10 +14,30 @@ function ExplorerSongPage () {
   const sessionUser = useSelector(state => state.session.user);
   const [showModal, setShowModal] = useState(false);
 
-  
+
 
   return (
-    <div>hello</div>
+    <div class="grid-container">
+      <div class="Header">
+        <div className='uploadlogout'>
+            <LogOutComponent/>
+        </div>
+        <div>
+          <p className="yourSongs">Explore Song Uploaded by Other Users!</p>
+        </div>
+      </div>
+      <div class="MainBody">
+          <RenderSongPage/>
+      </div>
+      <div class="MediaPlayer">
+          <AudioPlayer/>
+      </div>
+      <div class="SideBar">
+        <div class="sideBarWrapper">
+          <span><SideBar/></span>
+        </div>
+      </div>
+    </div>
   )
 }
 
